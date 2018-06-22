7506674e4981af7230df32a7be43300b85c0ec03
Alejandro Sabogal <asabogal.git@gmail.com>
2018-06-22 01:52:50 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -61,6 +61,7 @@ def can_be_found_updated_and_saved
   movie = Movie.find_by(title: "Awesome Flick")
   movie.update(title: "Even Awesomer Flick")
   movie.save
+end
 
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
